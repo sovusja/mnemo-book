b9dddb1c4fa39108cd5026f2009a3fdd4b1a84d3
VKravchuk <you@example.com>
2023-10-15 10:54:40 +0300

    Mnemo sqrt sound added

--- src/components/sqrtElement/SqrtElement.js
+++ src/components/sqrtElement/SqrtElement.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./SqrtElement.scss";
 
 const SqrtElement = (props) => {
   const { currentElementData } = props;
 
+  const soundData = currentElementData?.sound?.fields;
+  const audioRef = useRef(null);
+
+  const clickHandler = () => {
+    if (!soundData) {
+      return;
+    }
+    audioRef.current.play();
+  };
+
   return (
-    <div className="element-sqrt">
+    <div onClick={clickHandler} className="element-sqrt">
       <img
         className="element-sqrt__img"
         src={currentElementData.image.fields.file.url}
       />
       <h3 className="element-sqrt__text">{currentElementData.text}</h3>
+      {soundData && (
+        <audio ref={audioRef} controls>
+          <source src={soundData.file.url} type="audio/ogg"></source>
+        </audio>
+      )}
     </div>
   );
 };
